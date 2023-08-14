0d200ebd674b0291328bee0c936d615290b49906
EC2 Default User <ec2-user@ip-172-31-38-245.ap-northeast-1.compute.internal>
2023-08-14 08:08:14 +0000

    フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,14 @@ class BooksController < ApplicationController
 
 def create
   @book = Book.new(book_params)
-
-  @book.save
+  if @book.save
     redirect_to book_path(@book.id)
    flash[:notice] = "Book was successfully created. "
+ else
+   @books = Book.all
+   render :index
 end
+ end
 
   def index
     @books = Book.all
